3bcd88baaf07bc80bfb16dfa314cdaf3d1d54239
Gael <gael.touquet@gmail.com>
2018-06-19 09:52:23 +0200

    upgraded converttorecnnfiles.py to take ROOTfile as input

--- converttorecnnfiles.py
+++ converttorecnnfiles.py
@@ -15,10 +15,13 @@ output format :
 import numpy as np
 import sys
 import multiprocessing as mp
+from ROOT import TFile
+from root_numpy import tree2array
+from functools import partial
 
 
 
-def multithreadmap(f,X,ncores=20):
+def multithreadmap(f,X,ncores=20, **kwargs):
 	"""
 	multithreading map of a function, default on 20 cpu cores.
 	"""
@@ -54,36 +57,43 @@ def find_first_non_particle(jet):
 			last = m-1
 	return(m)
 
-def select_particle_features(jet):
+def select_particle_features(jet, addID=False):
 	"""
 	select E,px,py,pz of the particle.
 	"""
-	return(jet[:,[3,0,1,2,0]])
+        if addID:
+	        return(jet[:,[3,0,1,2,0]])
+        else:
+                return(jet[:,[3,0,1,2]])
 
 
-#Load file
-if len(sys.argv)>1:
-	input_file = sys.argv[1]
-else :
-	input_file = '/data/yohann/data_recnn/HiggsSUSYGG160_dataformat.npy'
+def converttorecnnfiles(input_file, addID=False):
+        #load data
+        f = TFile(input_file)
+        tree = f.Get('tree')
+        jets_array = tree2array(tree,'ptcs')
+        
+        #process
+        indexes = multithreadmap(find_first_non_particle, jets_array)
+        
+        jets_array = list(jets_array)
+        
+        for i in range(len(jets_array)):
+	        jets_array[i] = jets_array[i][:indexes[i]]
+                
+        select_particle_features_ID = partial(select_particle_features, addID=addID)
+        jets_array = multithreadmap(select_particle_features_ID,jets_array)
 
-#load data
-jets_array = np.load(input_file)
-
-#process
-jets_array = multithreadmap(extract_first_element, jets_array)
-
-
-indexes = multithreadmap(find_first_non_particle, jets_array)
-
-jets_array = list(jets_array)
-
-for i in range(len(jets_array)):
-	jets_array[i] = jets_array[i][:indexes[i]]
-
-
-jets_array = multithreadmap(select_particle_features,jets_array)
+        np.save(input_file[:input_file.find('.')]+'_processed_for_recnn',jets_array)
 
 
 
-np.save(input_file[:-4]+'_processed_for_recnn',jets_array)
+if __name__ == '__main__':
+        import argparse
+        parser = argparse.ArgumentParser(description='Convert Dataformated ROOTfiles to numpy files usable in the notebooks.')
+        parser.add_argument("rootfile", help="path to the ROOTfile to be converted",type=str)
+        # parser.add_argument("--branchlist", help="list of branches to keep", nargs='+') # for now respect naming scheme used to produce trees
+        parser.add_argument("--addID", help="wether or not to add the pdgID in the output", action="store_true")
+        args = parser.parse_args()
+        
+        converttorecnnfiles(args.rootfile, addID=args.addID)
