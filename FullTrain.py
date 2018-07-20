fd09c3750749689b77ea742179df273f640a6c5e
Yohann Faure <faure.yohann@gmail.com>
2018-07-20 18:13:44 +0200

    bug fix

--- FullTrain.py
+++ FullTrain.py
@@ -1,6 +1,6 @@
 from train import train
 
-typelist=["particle"]
+typelist=["0.3","1e-05"]
 
 filelist=["anti-kt","kt","random","seqpt","seqpt_reversed","cambridge"]
 
@@ -12,7 +12,7 @@ for i in typelist:
 		print("\n\n\n\n")
 		print(i,j)
 		print("\n\n\n\n")
-		nametrain="/data/conda/recnn/data/npyfilesregression/"+i+"_oriented_"+j+"_with_id_train.npy"
-		namemodel="/data/conda/recnn/data/modelsregression/"+i+"_oriented_"+j+"_with_id_model.pickle"
-		train(nametrain,namemodel,n_features=12,n_hidden=40,n_epochs=10,batch_size=64,step_size=0.001,decay=0.7,statlimit=50000,regression=True,verbose=True)
+		nametrain="/data/conda/recnn/data/npyfilesregression/Background_JEC_test_ID_preprocessed_R="+i+"_"+j+".npy"
+		namemodel="/data/conda/recnn/data/modelsregression/Model_R="+i+"_"+j+".pickle"
+		train(nametrain,namemodel,n_features=14,n_hidden=40,n_epochs=5,batch_size=64,step_size=0.001,decay=0.5,regression=True,verbose=True)
 
--- recnn/evaluate.py
+++ recnn/evaluate.py
@@ -3,9 +3,9 @@
 
 import numpy as np
 import pickle
-from recnn.recnn import grnn_predict_gated
-from recnn.preprocessing import apply_tf_transform
-from recnn.preprocessing import create_tf_transform
+from recnn import grnn_predict_gated
+from preprocessing import apply_tf_transform
+from preprocessing import create_tf_transform
 
 def compute_roc_curve(y, y_pred,density=1000):
     """return the roc curve"""
@@ -49,116 +49,3 @@ def build_roc(X, y, filename, func=None):
     print("ROC AUC = %.4f" % roc)
     return(roc, fpr, tpr)
 
-
-
-# In[]:
-
-Xtest,ytest=np.load('/data/conda/recnn/data/npyfilesregression/Background_JEC_test_ID_preprocessed_R=0.3_anti-kt.npy')
-Xtrain,_=np.load('/data/conda/recnn/data/npyfilesregression/Background_JEC_train_ID_preprocessed_R=0.3_anti-kt.npy')
-X=transform_for_prediction(Xtrain,Xtest)
-#xptgen=[x['genpt'] for x in X]
-xptgen=ytest
-xptraw=[x['pt'] for x in X]
-y = predict(X, '/data/conda/recnn/data/modelsregression/Background_JEC_train_ID_preprocessed_R=0.3_anti-kt_MorePreciseStat.pickle', func=grnn_predict_gated,regression = True)
-import matplotlib.pyplot as plt
-#indices = [i for i in range(len(X)) if -0.2<X[i]["eta"]<0.2]
-# In[]:
-fig=plt.figure()
-fig.set_size_inches((8,8))
-funct = np.array(xptraw)/np.array(xptgen)
-histraw,binsraw = np.histogram(funct,bins=1000,normed=True)
-width = (binsraw[1] - binsraw[0])
-center = (binsraw[:-1] + binsraw[1:]) / 2
-plt.grid()
-plt.xlim([0,3])
-mean =np.mean(funct)
-sigma=np.std(funct)
-plt.bar(center, histraw, align='center', width=width,label="ptraw/ptgen, mean ={:.4f}, std = {:.4f}".format(mean,sigma),alpha=0.5,color='green')
-
-mean =np.mean(funct)
-sigma=np.std(funct)
-#def f(x):
-#    return(max(histraw)*np.exp(-((x-mean)**2)/(2*(sigma**2))))
-#x0=np.linspace(0,3,10000)
-#plt.plot(x0,f(x0))
-
-###
-
-funct = np.array(y)/np.array(xptgen)
-histpred,binspred = np.histogram(funct,bins=100,normed=True)
-width = (binspred[1] - binspred[0])
-center = (binspred[:-1] + binspred[1:]) / 2
-
-plt.xlim([0,3])
-mean =np.mean(funct)
-sigma=np.std(funct)
-
-plt.bar(center, histpred, align='center', width=width,label="ptpred/ptgen, mean ={:.4f}, std = {:.4f}".format(mean,sigma),alpha=0.5,color='red')
-plt.grid(True)
-plt.ylabel('density')
-plt.xlabel('pt/ptgen')
-
-#def f(x):
-#    return(max(histpred)*np.exp(-((x-mean)**2)/(2*(sigma**2))))
-#x0=np.linspace(0,3,10000)
-#plt.plot(x0,f(x0))
-
-plt.legend()
-
-
-fig.savefig("figure_histogram_end_EtaReducedStat.png",dpi=600)
-
-
-
-# In[]:
-fig=plt.figure()
-fig.set_size_inches((8,8))
-funct = np.array(xptraw)
-histraw,binsraw = np.histogram(funct,bins=1000,normed=True)
-width = (binsraw[1] - binsraw[0])
-center = (binsraw[:-1] + binsraw[1:]) / 2
-plt.grid()
-mean =np.mean(funct)
-sigma=np.std(funct)
-plt.bar(center, histraw, align='center', width=width,label="ptraw, mean ={:.4f}, std = {:.4f}".format(mean,sigma),alpha=0.5,color='green')
-
-mean =np.mean(funct)
-sigma=np.std(funct)
-#def f(x):
-#    return(max(histraw)*np.exp(-((x-mean)**2)/(2*(sigma**2))))
-#x0=np.linspace(0,3,10000)
-#plt.plot(x0,f(x0))
-
-###
-
-funct = np.array(y)
-histpred,binspred = np.histogram(funct,bins=1000,normed=True)
-width = (binspred[1] - binspred[0])
-center = (binspred[:-1] + binspred[1:]) / 2
-
-mean =np.mean(funct)
-sigma=np.std(funct)
-
-plt.bar(center, histpred, align='center', width=width,label="ptpred, mean ={:.4f}, std = {:.4f}".format(mean,sigma),alpha=0.7,color='red')
-plt.grid(True)
-plt.ylabel('density')
-plt.xlabel('pt')
-
-#def f(x):
-#    return(max(histpred)*np.exp(-((x-mean)**2)/(2*(sigma**2))))
-#x0=np.linspace(0,3,10000)
-#plt.plot(x0,f(x0))
-
-
-
-plt.legend()
-
-
-
-fig.savefig("figure_histogram_distribution_EtaReducedStat.png",dpi=600)
-#histpred= numpy.histogram(y/xptgen,bins=100,normed=True)
-
-#plt.scatter(xpt,y/ytest,s=0.1);plt.grid();plt.show()
-#plt.hist(histraw,label='raw')
-#plt.hist(histpred,label='predicted')
-#plt.grid();plt.show()
