6185178d718ce559397a6c99f6b8f164c59b6db4
Gael <gael.touquet@gmail.com>
2018-10-03 15:36:33 +0200

    preprocessing modularisation

--- preprocess_for_training.py
+++ preprocess_for_training.py
@@ -23,7 +23,7 @@ get_ipython().magic(u'load_ext cython')
 get_ipython().run_cell_magic(u'cython', u'-f -+ -I/usr/local/include   -lm -L/usr/local/lib -lfastjettools -lfastjet -lfastjetplugins -lsiscone_spherical -lsiscone', u'import numpy as np\ncimport numpy as np\nnp.import_array()\n\nfrom libcpp.pair cimport pair\nfrom libcpp.vector cimport vector\n\ncdef extern from "/home/yohann/Desktop/stage/recnn/fj.cc":\n    void fj(vector[double]& a, \n            vector[vector[int]]& trees, \n            vector[vector[double]]& contents, \n            vector[double]& masses, \n            vector[double]& pts, \n            double R, int jet_algorithm)\n    \ncpdef cluster(np.ndarray[np.double_t, ndim = 2, mode = "c"] a, \n              R = 1., jet_algorithm = 0):\n    cdef vector[double] v\n    cdef vector[vector[int]] trees\n    cdef vector[vector[double]] contents\n    cdef vector[double] masses\n    cdef vector[double] pts \n    for value in a.ravel():\n        v.push_back(value)\n    \n    fj(v, trees, contents, masses, pts, R = R, jet_algorithm = jet_algorithm)\n    jets = []\n    \n    for tree, content, mass, pt in zip(trees, contents, masses, pts):\n        tree = np.array([e for e in tree]).reshape(-1, 2)\n        content = np.array([e for e in content]).reshape(-1, 5)\n        jets.append((tree, content, mass, pt))\n        \n    return jets')
 
 # In[]:
-def preprocess_for_training(filename,regression = False,R_clustering = 0.3,issignal = True):
+def preprocess_for_training(filename,regression = False,R_clustering = 0.3,issignal = True, tosavefilename=''):
     events = np.array(np.load(filename))
     signal = multithreadmap(create_jet_dictionary,events,cluster = cluster,regression = regression,R = 1000.)
     
@@ -35,13 +35,6 @@ def preprocess_for_training(filename,regression = False,R_clustering = 0.3,issig
             y = np.ones(len(X),dtype = int)
         else :
             y = np.zeros(len(X),dtype = int)
-    
-    ### Define paths ###
-    d = filename[::-1].find('/')
-    if regression :
-        tosavefilename = filename[:-d]+'npyfilesregression/'+filename[-d:-4]+'_preprocessed_R='+str(R_clustering)+'_'
-    else :
-        tosavefilename = filename[:-d]+'npyfiles/'+filename[-d:-4]+'_preprocessed_R='+str(R_clustering)+'_'
 
     print('### kt ###')     
     X_ = np.copy(X)
@@ -103,9 +96,7 @@ if __name__ == "__main__":
     parser.add_argument("--regression", help = "Add this to use regression data.", action = "store_true")
     parser.add_argument("--R_clustering", help = "0.3 for subjet, 0. for particle", type = float, default = 0.3)
     parser.add_argument("--issignal", help = "Add this if you are converting the signal sample of your discrimination set.", action = "store_true")
+    parser.add_argument("--tosavefilename", help = "")
     args = parser.parse_args()
 
-    if args.R_clustering == 0.:
-        preprocess_for_training(args.filename,regression = args.regression,R_clustering = 0.00001,signal = args.signal)
-    else :
-        preprocess_for_training(args.filename,regression = args.regression,R_clustering = args.R_clustering,issignal = args.issignal)
+    preprocess_for_training(args.filename,regression = args.regression,R_clustering = args.R_clustering,issignal = args.issignal, tosavefilename = args.tosavefilename)
